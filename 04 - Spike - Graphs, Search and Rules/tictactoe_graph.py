724f27b18e0dc23168b3ca95999e31d4ea339c57
Ben Holmes <103024841@student.swin.edu.au>
2024-03-27 16:16:06 +1100

    added inital spike report and inital attempt at board graph

--- 04 - Spike - Graphs, Search and Rules/tictactoe_graph.py
+++ 04 - Spike - Graphs, Search and Rules/tictactoe_graph.py
@@ -1,4 +1,4 @@
-'''Tic-Tac-Toe, Object Oriented Version
+'''Tic-Tac-Toe
 
 Created for COS30002 AI for Games, Lab,
 by Clinton Woodward <cwoodward@swin.edu.au>
@@ -6,155 +6,198 @@ by Clinton Woodward <cwoodward@swin.edu.au>
 For class use only. Do not publically share or post this code without
 permission.
 
-Notes/Tips:
-* Exactly the same behaviour as the function based version (tictactoe_cli.py).
-* Very simple single class conversion of functions and global variables
-* Game loop still controlled by the three input/update/render methods
-* Internal class helper functions are marked by a leading "_"
+Notes:
+* Formatted with 4-space indentation
+* This simple function based implementation does not use an OO design.
+* Each function has a description string -- read to know more.
+* Overall game flow follows a standard game loop trinity:
+    - process_input() # from the current player (human/AI)
+    - update_mode()   # check the players input, then update the game world
+    - render_board()  # draw the current game board
+* Global variable (oh no!) are used to store and share game related data.
+* You need to be able to interact with this code when it runs. Either run
+  from a terminal or make sure your tool allows you to enter input as the
+  script runs.
 
-General Python Notes:
-* All class methods have "self" as their first parameter
-* All class variables/methods need to be accessed by a leading "self."
+If you want to create your own AI it is suggested that you:
+* Copy the get_ai_move function and rename it.
+* Write your own new fancy AI thinking code
+* Update the "process_input" function to call your new "get_ai_move" code.
+
+Want OO? There's another version of this code. Same functions, with nice
+classes.
+
+Updates:
+2019-03-12: python 2 to 3 (mainly print() changes, and raw_input to input())
 
 '''
 
 from random import randrange
 
+# static game data - doesn't change (hence immutable tuple data type)
+WIN_SET = (
+    (0, 1, 2), (3, 4, 5), (6, 7, 8), (0, 3, 6),
+    (1, 4, 7), (2, 5, 8), (0, 4, 8), (2, 4, 6)
+)
 
-class TicTacToe(object):
-    # class variables - belong to the *class* - NOT object instance.
-    WIN_SET = (
-        (0, 1, 2), (3, 4, 5), (6, 7, 8), (0, 3, 6),
-        (1, 4, 7), (2, 5, 8), (0, 4, 8), (2, 4, 6)
-    )
+# global variables for game data
+board = [' '] * 9
+current_player = ''  # 'x' or 'o' for first and second player
 
-    HR = '-' * 40
+players = {
+    'x': 'Human',
+    'o': 'Super AI',  # final comma is optional, but doesn't hurt
+}
 
-    def __init__(self):
-        # class instance variables for game data (were global variables)
-        self.board = [' '] * 9
-        self.players = {'x': 'Human', 'o': 'Super AI' }
-        self.winner = None
-        self.move = None
-        # by default the human player starts. This could be random or a choice.
-        self.current_player = 'x'
-        # Welcome ...
-        print('Welcome to the amazing+awesome tic-tac-toe!')
-        # Show help (number) details
-        self.show_human_help()
-        # show the initial board and the current player's move
-        self.render_board()
+winner = None
+move = None
 
-    def _check_move(self):
-        '''This function will return True if ``move`` is valid (in the board range
-        and free cell), or print an error message and return False if not valid.
-        ``move`` is an int board position [0..8].
-        '''
-        try:
-            self.move = int(self.move)
-            if self.board[self.move] == ' ':
-                return True
-            else:
-                print('>> Sorry - that position is already taken!')
-                return False
-        except:  # bare except bad practice but works
-            print('>> %s is not a valid position! Must be int between 0 and 8.' % self.move)
+# "horizontal rule", printed as a separator ...
+HR = '-' * 40
+
+
+#==============================================================================
+# Game model functions
+
+def check_move():
+    '''This function will return True if ``move`` is valid (in the board range
+    and free cell), or print an error message and return False if not valid.
+    ``move`` is an int board position [0..8].
+    '''
+    global move
+    try:
+        move = int(move)
+        if board[move] == ' ':
+            return True
+        else:
+            print('>> Sorry - that position is already taken!')
             return False
+    except:  # a "bare" except is bad practice, but simple and works still
+        print('>> %s is not a valid position! Must be int between 0 and 8.' % move)
+        return False
 
-    def _check_for_result(self):
-        '''Checks the current board to see if there is a winner, tie or not.
-        Returns a 'x' or 'o' to indicate a winner, 'tie' for a stale-mate game, or
-        simply False if the game is still going.
-        '''
-        board = self.board
-        for row in self.WIN_SET:
-            if board[row[0]] == board[row[1]] == board[row[2]] != ' ':
-                return board[row[0]] # return an 'x' or 'o' to indicate winner
 
-        if ' ' not in board:
-            return 'tie'
+def check_for_result():
+    '''Checks the current board to see if there is a winner, tie or not.
+    Returns a 'x' or 'o' to indicate a winner, 'tie' for a stale-mate game, or
+    simply False if the game is still going.
+    '''
+    for row in WIN_SET:
+        if board[row[0]] == board[row[1]] == board[row[2]] != ' ':
+            return board[row[0]]  # return an 'x' or 'o' to indicate winner
 
-        return None
+    if ' ' not in board:
+        return 'tie'
 
-    #===========================================================================
-    # agent (human or AI) functions
+    return None
 
-    def get_human_move(self):
-        '''Get a human players raw input '''
-        return input('[0-8] >> ')
 
-    def get_ai_move(self):
-        '''Get the AI's next move '''
-        # A simple dumb random move - valid or NOT!
-        # Note: It is the models responsibility to check for valid moves...
-        return randrange(9) # [0..8]
+#==============================================================================
+# agent (human or AI) functions
 
-    #===========================================================================
-    # Standard trinity of game loop methods (functions)
 
-    def process_input(self):
-        '''Get the current players next move.'''
-        if self.current_player == 'x':
-            self.move = self.get_human_move()
+def get_human_move():
+    '''Get a human players raw input. Returns None if a number is not entered.'''
+    return input('[0-8] >> ')
+
+def get_ai_move():
+    '''Get the Ai's next move'''
+    # old random method atm
+    return randrange(9)
+
+#==============================================================================
+# Task 4 ai and board stuff
+
+def get_ai_move_spike():
+    # random AI for task 4
+    board_move = get_board_state()
+    if board_move == None:
+        return randrange(9)
+    else:
+        return board_move
+
+def get_board_state():
+    # task 4 possible board graph
+    i = 0
+    while i < 9:
+        board_root = board[:]
+        if board_root[i] == ' ':
+            board_root[i] = current_player
+            for row in WIN_SET:
+                if board_root[row[0]] == board_root[row[1]] == board_root[row[2]] != ' ':
+                    return i
+            i += 1
         else:
-            self.move = self.get_ai_move()
+            i += 1
+            continue
+    return None
+            
+#==============================================================================
+# Standard trinity of game loop methods (functions)
 
-    def update_model(self):
-        '''If the current players input is a valid move, update the board and check
-        the game model for a winning player. If the game is still going, change the
-        current player and continue. If the input was not valid, let the player
-        have another go.
-        '''
-        if self._check_move():
-            # do the new move (which is stored in the instance 'move' variable)
-            self.board[self.move] = self.current_player
-            # check board for winner (now that it's been updated)
-            self.winner = self._check_for_result()
-            # change the current player (regardless of the outcome)
-            if self.current_player == 'x':
-                self.current_player = 'o'
-            else:
-                self.current_player = 'x'
+def process_input():
+    '''Get the current players next move.'''
+    # save the next move into a global variable
+    global move
+    if current_player == 'x':
+        move = get_human_move()
+    else:
+        move = get_ai_move()
+
+
+def update_model():
+    '''If the current players input is a valid move, update the board and check
+    the game model for a winning player. If the game is still going, change the
+    current player and continue. If the input was not valid, let the player
+    have another go.
+    '''
+    global winner, current_player
+
+    if check_move():
+        # do the new move (which is stored in the global 'move' variable)
+        board[move] = current_player
+        # check board for winner (now that it's been updated)
+        winner = check_for_result()
+        # change the current player (regardless of the outcome)
+        if current_player == 'x':
+            current_player = 'o'
         else:
-            print('Try again')
+            current_player = 'x'
+    else:
+        print('Try again')
 
-    def render_board(self):
-        '''Display the current game board to screen.'''
-        board = self.board
-        print('    %s | %s | %s' % tuple(board[:3]))
-        print('   -----------')
-        print('    %s | %s | %s' % tuple(board[3:6]))
-        print('   -----------')
-        print('    %s | %s | %s' % tuple(board[6:]))
 
-        # pretty print the current player name
-        if self.winner is None:
-            print('The current player is: %s' % self.players[self.current_player])
+def render_board():
+    '''Display the current game board to screen.'''
 
-    def show_human_help(self):
-        '''Show the player help/instructions. '''
-        tmp = '''\
-    To make a move enter a number between 0 - 8 and press enter.
-    The number corresponds to a board position as illustrated:
+    print('    %s | %s | %s' % tuple(board[:3]))
+    print('   -----------')
+    print('    %s | %s | %s' % tuple(board[3:6]))
+    print('   -----------')
+    print('    %s | %s | %s' % tuple(board[6:]))
 
-        0 | 1 | 2
-        ---------
-        3 | 4 | 5
-        ---------
-        6 | 7 | 8
-        '''
-        print(tmp)
-        print(self.HR)
+    # pretty print the current player name
+    if winner is None:
+        print('The current player is: %s' % players[current_player])
 
-    def show_gameresult(self):
-        '''Show the game result winner/tie details'''
-        print(self.HR)
-        if self.winner == 'tie':
-            print('TIE!')
-        else:
-            print('%s is the WINNER!!!' % self.players[self.winner])
-        print(self.HR)
-        print('Game over. Goodbye')
+
+#==============================================================================
+
+
+def show_human_help():
+    '''Show the player help/instructions. '''
+    tmp = '''
+To make a move enter a number between 0 - 8 and press enter.
+The number corresponds to a board position as illustrated:
+
+    0 | 1 | 2
+    ---------
+    3 | 4 | 5
+    ---------
+    6 | 7 | 8
+    '''
+    print(tmp)
+    print(HR)
 
 
 #==============================================================================
@@ -164,14 +207,27 @@ class TicTacToe(object):
 
 
 if __name__ == '__main__':
-    # create instance (~ "new") object of type TicTacToe class
-    game = TicTacToe()
+    # Welcome ...
+    print('Welcome to the amazing+awesome tic-tac-toe!')
+    show_human_help()
+
+    # by default the human player starts. This could be random or a choice.
+    current_player = 'x'
+
+    # show the initial board and the current player's move
+    render_board()
 
     # Standard game loop structure
-    while game.winner is None:
-        game.process_input()
-        game.update_model()
-        game.render_board()
+    while winner is None:
+        process_input()
+        update_model()
+        render_board()
 
     # Some pretty messages for the result
-    game.show_gameresult()
+    print(HR)
+    if winner == 'tie':
+        print('TIE!')
+    elif winner in players:
+        print('%s is the WINNER!!!' % players[winner])
+    print(HR)
+    print('Game over. Goodbye')
