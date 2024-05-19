66e122f81b4581883ad6b6258c35b3ac8e59e496
Ben Holmes <103024841@student.swin.edu.au>
2024-05-19 16:33:48 +1000

    removal of uneeded functions, tested alternate move orders to see if that would effect it, tested other values to see what would happen

--- 08 - Spike - Goal-Oriented Action Planning (GOAP)/GOAP.py
+++ 08 - Spike - Goal-Oriented Action Planning (GOAP)/GOAP.py
@@ -47,19 +47,19 @@ path_chosen = []
 
 # Global goals with initial values
 goals = {
-    'Energy': 10,
-    'Hunger': 10,
+    'Energy': 20,
+    'Hunger': 20,
     'Fitness': 20,
 }
 
 # Global (read-only) actions and effects
 actions = {
-    'cook and eat food': {  'Energy': 2 ,'Hunger': -3 , 'Fitness': 0},
-    'get snack': {  'Energy': 1 , 'Hunger': -2 , 'Fitness': 2 },
     'go to sleep': { 'Energy': -3 , 'Hunger': 1 , 'Fitness': 0 },
     'have a nap': { 'Energy': -1, 'Hunger': 0 , 'Fitness': 0  },
     'go for a walk': { 'Energy': 1, 'Hunger': 0 , 'Fitness': -2 },
     'go to the gym': { 'Energy': 2, 'Hunger': 1 , 'Fitness': -4 },
+    'cook and eat food': {  'Energy': 2 ,'Hunger': -3 , 'Fitness': 0},
+    'get snack': {  'Energy': 1 , 'Hunger': -2 , 'Fitness': 2 },
 }
 
 class Path(object):
@@ -76,70 +76,6 @@ def apply_action(action):
     '''
     for goal, change in actions[action].items():
         goals[goal] = max(goals[goal] + change, 0)
-
-
-def action_utility(action, goal):
-    '''Return the 'value' of using "action" to achieve "goal".
-
-    For example::
-        action_utility('get raw food', 'Eat')
-
-    returns a number representing the effect that getting raw food has on our
-    'Eat' goal. Larger (more positive) numbers mean the action is more
-    beneficial.
-    '''
-    ### Simple version - the utility is the change to the specified goal
-
-    if goal in actions[action]:
-        # Is the goal affected by the specified action?
-        return -actions[action][goal]
-    else:
-        # It isn't, so utility is zero.
-        return 0
-
-def choose_action():
-    '''Return the best action to respond to the current most insistent goal.
-    '''
-    assert len(goals) > 0, 'Need at least one goal'
-    assert len(actions) > 0, 'Need at least one action'
-
-    # Find the most insistent goal - the 'Pythonic' way...
-    best_goal, best_goal_value = max(goals.items(), key=lambda item: item[1])
-
-    # ...or the non-Pythonic way. (This code is identical to the line above.)
-    #best_goal = None
-    #for key, value in goals.items():
-    #    if best_goal is None or value > goals[best_goal]:
-    #        best_goal = key
-
-    if VERBOSE: print('BEST_GOAL:', best_goal, goals[best_goal])
-
-    # Find the best (highest utility) action to take.
-    # (Not the Pythonic way... but you can change it if you like / want to learn)
-    best_action = None
-    best_utility = None
-    for key, value in actions.items():
-        # Note, at this point:
-        #  - "key" is the action as a string,
-        #  - "value" is a dict of goal changes (see line 35)
-
-        # Does this action change the "best goal" we need to change?
-        if best_goal in value:
-
-            # Do we currently have a "best action" to try? If not, use this one
-            if best_action is None:
-                best_action = key
-                best_utility = action_utility(best_action,best_goal)
-
-            # Is this new action better than the current action?
-            else:
-                utility = action_utility(key,best_goal)
-                if(utility > best_utility):
-                    best_action = key
-                    best_utility = utility
-
-    # Return the "best action"
-    return best_action
 
 def choose_action_path():
     '''chose an action path and return current action on that path'''
@@ -162,8 +98,6 @@ def choose_action_path():
 
         path_chosen = paths[path_number]
         
-    print('PATH MOVES:',path_chosen.moves)
-    print('PATH GOALS:',path_chosen.goals)
     move_amount = len(path_chosen.moves) - 1
 
     apply_action(path_chosen.moves[move_counter])
@@ -211,8 +145,6 @@ def path_apply_action(action,path_goals):
 
 def print_actions():
     print('ACTIONS:')
-    # for name, effects in list(actions.items()):
-    #     print(" * [%s]: %s" % (name, str(effects)))
     for name, effects in actions.items():
         print(" * [%s]: %s" % (name, str(effects)))
 
@@ -227,8 +159,10 @@ def run_until_all_goals_zero():
     while running:
         print('MOVE NUMBER:', move_number)
         print('GOALS:', goals)
-        # What is the best action
+        # What is the best action path
         action = choose_action_path()
+        print('PATH MOVES:',path_chosen.moves)
+        print('PATH GOALS:',path_chosen.goals)
         print('PATH MOVE NUMBER:', move_counter + 1)
         print('BEST ACTION:', action)
         # Apply the best action
